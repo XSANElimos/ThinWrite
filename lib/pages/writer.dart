531aa1d1ab41e2b885c4ecf581be4c741882f25b
alikonus <i_zhangyuanwei@cvte.com>
2023-02-23 09:32:47 +0800

    add rich editor

--- lib/pages/writer.dart
+++ lib/pages/writer.dart
@@ -8,39 +8,53 @@ class WriterPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Column(
-      children: <Widget>[
-        Row(
-          children: <Widget>[
-            InkWell(
-              onTap: () {},
-              child: Chip(
-                  onDeleted: () {},
-                  avatar: const Icon(Icons.text_format_rounded),
-                  label: const Text('UnderLine')),
-            )
-          ],
-        ),
-        Expanded(
-            child: RichEditor(
-          key: GlobalKey(),
-          value: 'initial html here',
-          editorOptions: RichEditorOptions(
-            placeholder: 'Start typing',
-            padding: EdgeInsets.symmetric(horizontal: 5.0),
-            baseFontFamily: 'sans-serif',
-            barPosition: BarPosition.TOP,
+    return SafeArea(
+      child: Scaffold(
+          body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              prototypeItem: InkWell(
+                onTap: () {},
+                child: Chip(
+                    onDeleted: () {},
+                    avatar: const Icon(Icons.text_format_rounded),
+                    label: const Text('UnderLine')),
+              ),
+              children: <Widget>[
+                for (int i = 0; i < 5; i++)
+                  InkWell(
+                    onTap: () {},
+                    child: Chip(
+                        onDeleted: () {},
+                        avatar: const Icon(Icons.text_format_rounded),
+                        label: const Text('UnderLine')),
+                  )
+              ],
+            ),
           ),
-          getImageUrl: (image) {
-            String link =
-                'https://avatars.githubusercontent.com/u/24323581?v=4';
-            String base64 = base64Encode(image.readAsBytesSync());
-            String base64String = 'data:image/png;base64, $base64';
-            return base64String;
-          },
-        ))
-      ],
-    ));
+          Expanded(
+              child: RichEditor(
+            key: GlobalKey(),
+            value: '',
+            editorOptions: RichEditorOptions(
+              placeholder: 'Start typing',
+              padding: EdgeInsets.symmetric(horizontal: 5.0),
+              baseFontFamily: 'sans-serif',
+              barPosition: BarPosition.TOP,
+            ),
+            getImageUrl: (image) {
+              String link =
+                  'https://avatars.githubusercontent.com/u/24323581?v=4';
+              String base64 = base64Encode(image.readAsBytesSync());
+              String base64String = 'data:image/png;base64, $base64';
+              return base64String;
+            },
+          ))
+        ],
+      )),
+    );
   }
 }
